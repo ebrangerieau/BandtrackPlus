86710018607c12899ece31cb10ffbdd0ef1bf972
ebrangerieau <ebrangerieau@gmail.com>
2025-09-04 22:06:19 +0200

    Read host from HOST env var

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from bandtrack.api import run_server
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Run the BandTrack server")
-    parser.add_argument("--host", default="0.0.0.0")
+    parser.add_argument("--host", default=os.environ.get("HOST", "0.0.0.0"))
     parser.add_argument("--port", type=int, default=int(os.environ.get("PORT", 8080)))
     args = parser.parse_args()
     run_server(args.host, args.port)
